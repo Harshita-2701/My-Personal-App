4e16780304da187fe84c20c2884beb94eb011a41
rishik <rishikraj990@gmail.com>
2024-02-02 22:38:34 +0530

    added to manifest file

--- app/src/main/java/com/harshik/mypersonalapp/mainApp/WelcomeScreenFirstTime.java
+++ app/src/main/java/com/harshik/mypersonalapp/mainApp/WelcomeScreenFirstTime.java
@@ -3,6 +3,8 @@ package com.harshik.mypersonalapp.mainApp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.harshik.mypersonalapp.R;
@@ -13,6 +15,14 @@ public class WelcomeScreenFirstTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen_first_time);
+
+        String appName = getString(R.string.app_name);
+        TextView appNameTextView = findViewById(R.id.app_name_textView);
+        appNameTextView.setText(appName);
+
+        String one_time_disc = getString(R.string.one_time_disc);
+        TextView dicriptionTextView = findViewById(R.id.dicription_textView);
+        dicriptionTextView.setText(one_time_disc);
 
         // Find views in the layout
         Button one_time_button = findViewById(R.id.one_time_button);
